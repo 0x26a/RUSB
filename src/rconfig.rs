36289c369e031e51dbf643b58f43e8cdae843f2c
0x26a <65681383+0x26a@users.noreply.github.com>
2025-04-12 19:57:16 +0200

    Update rconfig.rs

--- src/rconfig.rs
+++ src/rconfig.rs
@@ -1,4 +1,4 @@
-pub const EFI_APP_PATH: &str ="efi\\boot\\bootx64.efi"; // to use to run on qemu
+pub const EFI_APP_PATH: &str ="efi\\boot\\bootx64.efi"; // to use if you want it to load itself
 
 pub const PICTURE_WIDTH: u32 = 720;
 pub const PICTURE_SIZE: usize = 956880;
